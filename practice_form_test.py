58d0b7ab2ac0ec316cfa211247099fd0e9946c24
mtx <m33wwe@gmail.com>
2023-04-16 11:39:35 +0300

    add subj form fillings, improve performance

--- practice_form_test.py
+++ practice_form_test.py
@@ -1,19 +1,18 @@
 import os
 from selene import have, command
 from selene.support.shared import browser
-from selenium.webdriver.common.by import By
 
-def test_successful_input_Registration_Form(self=None):
+
+def test_successful_input_registration_form():
     browser.open('https://demoqa.com/automation-practice-form')
     browser.driver.maximize_window()
 
-    browser.element('[id=firstName]').type('Michael')
-    browser.element('[id=lastName]').type('Kors')
+    browser.element('[id = firstName]').type('Michael')
+    browser.element('[id = lastName]').type('Kors')
     browser.element('[id = userEmail]').type('Michael@Kors.com')
-    browser.element('[name=gender][value=Male]').double_click()
+    browser.element('[name = gender][value=Male]').double_click()
     browser.element('[id = userNumber]').type('8667095677')
 
-    # Date of Birth
     browser.element('#dateOfBirthInput').press()
     browser.element('.react-datepicker__month-select').click()
     browser.element('option[value="7"]').click()
@@ -21,21 +20,20 @@ def test_successful_input_Registration_Form(self=None):
     browser.element('option[value="1959"]').click()
     browser.element('div[aria-label="Choose Sunday, August 9th, 1959"]').click()
 
+    browser.element('#subjectsInput').type("Chemistry").press_enter()
+    browser.element('#subjectsInput').type("Maths").press_enter()
+    browser.element('#subjectsInput').type("Physics").press_enter()
+
     browser.element('label[for="hobbies-checkbox-1"]').click()
     browser.element('label[for="hobbies-checkbox-2"]').click()
     browser.element('#uploadPicture').type(os.getcwd()+"/solution.png")
     browser.element('[id = currentAddress]').type('Rodriguez side, LA 93111')
 
-    #State and City
     browser.element('#state').perform(command.js.scroll_into_view)
     browser.element('#state').click()
-
     browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('Uttar Pradesh')).click()
-    # browser.all('[id^=react-select-4-input][id*=option]').click()
-    # browser.all('[id^=react-select-4-input][id*=option]').element_by(have.exact_text('Agra'))
+    browser.element('#react-select-4-input').type('Agra').press_enter()
 
-
-    browser.element('[id=submit]').click()
+    browser.element('#submit').perform(command.js.click)
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     browser.element('[id =closeLargeModal]').click()
-
-
